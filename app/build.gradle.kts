3de4a5746a09324adba0f81faf9bf50cab4c31d9
Victor Mican <victor@gmail.com>
2025-07-24 12:28:00 -0500

    Correción Funcionamiento

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,12 +27,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildToolsVersion = "36.0.0"
 }
 
 dependencies {
